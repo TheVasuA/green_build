5aec548f96860372c21d486304571ed0f2c99b71
TheVasuA <thevasu.a@gmail.com>
2025-10-28 12:06:33 +0530

    green builders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
 
-
-function App() {
-
-
+const App = () => {
   return (
     <>
-     <h1 >react</h1>
-    </>
-  )
-}
+      <Navbar />
+      <HeroSection />
+      <div className=" max-w-7xl mx-auto pt-14 md:pt-10 px-6">
+    
+        <FeatureSection />
+        <Workflow />
+   
+        <Testimonials />
+        <Footer />
+      </div>
 
-export default App
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,85 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { navItems } from "../constants";
+import { BiMenu, BiPhoneCall } from "react-icons/bi";
+
+const Navbar = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
+
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container px-4 mx-auto relative lg:text-sm">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
+            <span className="text-2xl tracking-tight bg-gradient-to-r from-green-600 via-yellow-500 to-red-400 inline-block text-transparent bg-clip-text ">Green Build Enterprises</span>
+          </div>
+          <ul className="hidden lg:flex ml-14 space-x-12">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+
+          <div className="hidden lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Gallery
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Contact Us
+            </a>
+            <div className="flex items-center gap-4 hidden lg:flex list-none">
+              <li>
+                <BiPhoneCall className="h-[50px] w-[50px] rounded-md bg-primary p-2 text-2xl text-white hover:bg-primary/90" />
+              </li>
+              <li>
+                <div>
+                  <p className="text-sm">Call us on</p>
+                  <p className="text-md">+91 9071285992</p>
+                </div>
+              </li>
+            </div>
+          </div>
+          <div className="lg:hidden md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
+          </div>
+        </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Gallery
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Contact Us
+              </a>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
